bd22c580d4ae2b365279e36168d4731305281d21
Noemi Muste <nmustem@gmail.com>
2024-05-20 21:59:19 +0200

    Adds an input text with a button to add a task and it outputs it as a list

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const [enteredTaskText, setEnteredTaskText] = useState('');
+  const [courseTasks, setCourseTasks] = useState([]);
+
+  function goalInputHandler(enteredText) {
+    setEnteredTaskText(enteredText);
+  };
+
+  function addTaskHandler() {
+    setCourseTasks((currentCourseTasks) => [...currentCourseTasks, enteredTaskText]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder='Your TODO list' onChangeText={goalInputHandler} />
+        <Button title='Add a task' onPress={addTaskHandler} />
+      </View>
+      <View style={styles.tasksContainer}>
+        {courseTasks.map((goal) => (
+          <View key={goal} style={styles.taskItem}>
+            <Text style={styles.taskText}>{goal}</Text>
+          </View>
+        ))}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 16,
   },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderColor: '#cccccc',
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '70%',
+    marginRight: 8,
+    padding: 8,
+  },
+  tasksContainer: {
+    flex: 5,
+  },
+  taskItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: '#5e0acc',
+  },
+  taskText: {
+    color: 'white'
+  }
 });
