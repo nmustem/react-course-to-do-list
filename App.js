a7f981eae8330e3e5e682460c5be14d1a9630c8e
Noemi Muste <nmustem@gmail.com>
2024-05-27 18:14:23 +0200

    Exports logic to TaskItem component

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
-import { Button, FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, FlatList, StyleSheet, TextInput, View } from 'react-native';
+import TaskItem from './components/TaskItem';
 
 export default function App() {
   const [enteredTaskText, setEnteredTaskText] = useState('');
@@ -23,35 +24,16 @@ export default function App() {
         <Button title='Add a task' onPress={addTaskHandler} />
       </View>
       <View style={styles.tasksContainer}>
-        {/*<FlatList> --> is specifically built for scrollable lists. Internally it will only render de items that are visible
-          and all the items that are off screen will only be loaded an rendered lazily as they are needed because the user is scrolling */}
-
         <FlatList
           data={courseTasks}
           renderItem={(itemData) => {
-            return (
-              <View style={styles.taskItem}>
-                <Text style={styles.taskText}>{itemData.item.text}</Text>
-              </View>
-            );
+            return <TaskItem text={itemData.item.text} />;
           }}
           keyExtractor={(item, index) => {
             return item.id;
           }}
           alwaysBounceVertical={false}
         />
-        {/* El keyExtractor no és necessari si els objectes de la llista tenen una propietat anomenada key */}
-
-        {/* <ScrollView> --> we do not want to use ScrollView for a list that can be really long, as it could lead us to 
-            performance issues. ScrollView is perfect for an article for example, that we know the space it´s gonna take aprox
-          
-          <ScrollView>
-            {courseTasks.map((goal) => (
-              <View key={goal} style={styles.taskItem}>
-                <Text style={styles.taskText}>{goal}</Text>
-              </View>
-          </ScrollView>
-        */}
       </View>
     </View>
   );
@@ -82,13 +64,4 @@ const styles = StyleSheet.create({
   tasksContainer: {
     flex: 5,
   },
-  taskItem: {
-    margin: 8,
-    padding: 8,
-    borderRadius: 6,
-    backgroundColor: '#5e0acc',
-  },
-  taskText: {
-    color: 'white'
-  }
 });
