86bf6aca3b2980608f09c993baca82f28aed60a7
Noemi Muste <nmustem@gmail.com>
2024-05-29 17:25:46 +0200

    Adds android ripple effect and an ios alternative

--- components/TaskItem.js
+++ components/TaskItem.js
@@ -2,24 +2,31 @@ import { Pressable, StyleSheet, Text, View } from "react-native";
 
 function TaskItem(props) {
     return (
-        <Pressable onPress={props.onDeleteItem.bind(this, props.id)} >
-            <View style={styles.taskItem}>
+        <View style={styles.taskItem}>
+            <Pressable
+                android_ripple={{ color: '#87ACFD'}}
+                onPress={props.onDeleteItem.bind(this, props.id)}
+                style={({pressed}) => pressed && styles.pressedItem}
+            >
                 <Text style={styles.taskText}>{props.text}</Text>
-            </View>
-        </Pressable>
+            </Pressable>
+        </View>
     );
 };
 
 export default TaskItem;
 
 const styles = StyleSheet.create({
+    pressedItem: {
+        opacity: 0.5
+    },
     taskItem: {
         margin: 8,
-        padding: 8,
         borderRadius: 6,
-        backgroundColor: '#5e0acc',
+        backgroundColor: '#1B4CB7',
     },
     taskText: {
-        color: 'white'
+        color: 'white',
+        padding: 8,
     }
 });
